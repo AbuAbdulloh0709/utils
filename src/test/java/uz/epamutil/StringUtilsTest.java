056a6edf7f6e9f3afbc7afca113ba1b278f078e3
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-11-28 20:05:12 +0500

    fix test and gradle

--- src/test/java/uz/epamutil/StringUtilsTest.java
+++ src/test/java/uz/epamutil/StringUtilsTest.java
@@ -2,6 +2,7 @@ package uz.epamutil;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringUtilsTest {
@@ -9,11 +10,10 @@ class StringUtilsTest {
     public void isPositiveNumber() {
         String str1 = "-1614";
         boolean isPositive1 = StringUtils.isPositiveNumber(str1);
-        assertTrue(isPositive1, "It should be a positive number");
+        assertFalse(isPositive1, "It should be a non-positive number");
 
-        String str2 = "16145.2";
+        String str2 = "16145";
         boolean isPositive2 = StringUtils.isPositiveNumber(str2);
-        assertTrue(isPositive2, "It is positive number");
-
+        assertTrue(isPositive2, "It should be a positive number");
     }
 }
